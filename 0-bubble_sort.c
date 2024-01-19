f43eb8323a39af610af80e2e2513671ee7f2c5db
ikeali <ikechukwualisigwe01@mail.com>
2024-01-19 02:32:08 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,29 +6,29 @@
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
-void bubble_sort(int *array, size_t size)
-{
-    size_t i, j;
-    int temp;
-    int swapped;
+	void bubble_sort(int *array, size_t size)
+	{
+	size_t i, j;
+	int temp;
+	int swapped;
 
-    for (i = 0; i < size - 1; ++i)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; ++j)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
+	for (i = 0; i < size - 1; ++i)
+	{
+	swapped = 0;
+	for (j = 0; j < size - i - 1; ++j)
+	{
+	if (array[j] > array[j + 1])
+	{
+	temp = array[j];
+	array[j] = array[j + 1];
+	array[j + 1] = temp;
+	swapped = 1;
+	print_array(array, size);
+	}
+	}
 
-        if (!swapped)
-            break;
-    }
-}
+	if (!swapped)
+	break;
+	}
+	}
 
