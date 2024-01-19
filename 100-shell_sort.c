4537e1a4ded382874c0098d8e5cbf30b908e1987
ikeali <ikechukwualisigwe01@mail.com>
2024-01-19 02:48:38 -0800

    100-shell.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,35 +7,35 @@
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
-void shell_sort(int *array, size_t size)
-{
-    size_t interval = 1, i, j;
-    int temp;
+	void shell_sort(int *array, size_t size)
+	{
+	size_t interval = 1, i, j;
+	int temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+	return;
 
-    while (interval < size / 3)
-        interval = interval * 3 + 1;
+	while (interval < size / 3)
+	interval = interval * 3 + 1;
 
-    while (interval > 0)
-    {
-        for (i = interval; i < size; ++i)
-        {
-            temp = array[i];
-            j = i;
+	while (interval > 0)
+	{
+	for (i = interval; i < size; ++i)
+	{
+	temp = array[i];
+	j = i;
 
-            while (j >= interval && array[j - interval] > temp)
-            {
-                array[j] = array[j - interval];
-                j -= interval;
-            }
+	while (j >= interval && array[j - interval] > temp)
+	{
+	array[j] = array[j - interval];
+	j -= interval;
+	}
 
-            array[j] = temp;
-        }
+	array[j] = temp;
+	}
 
-        print_array(array, size);
-        interval /= 3;
-    }
-}
+	print_array(array, size);
+		interval /= 3;
+	}
+	}
 
