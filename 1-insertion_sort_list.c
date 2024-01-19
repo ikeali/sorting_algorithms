326c79ecb2e52dbb78c56dae205dec3dca73dcd0
ikeali <ikechukwualisigwe01@mail.com>
2024-01-19 02:08:12 -0800

    1-insertion_sort_list.c, 1-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,37 +6,37 @@
  *
  * @list: Pointer to the head of the doubly linked list
  */
-void insertion_sort_list(listint_t **list)
-{
-    listint_t *current, *temp;
+	void insertion_sort_list(listint_t **list)
+	{
+	listint_t *current, *temp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+	return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        temp = current->next;
+	while (current)
+	{
+	temp = current->next;
 
-        while (current->prev && current->n < current->prev->n)
-        {
-            if (current->next)
-                current->next->prev = current->prev;
+	while (current->prev && current->n < current->prev->n)
+	{
+	if (current->next)
+	current->next->prev = current->prev;
 
-            current->prev->next = current->next;
-            current->next = current->prev;
-            current->prev = current->prev->prev;
+	current->prev->next = current->next;
+	current->next = current->prev;
+	current->prev = current->prev->prev;
 
-            if (current->prev)
-                current->prev->next = current;
-            else
-                *list = current;
+	if (current->prev)
+	current->prev->next = current;
+	else
+	*list = current;
 
-            print_list(*list);
-        }
+	print_list(*list);
+	}
 
-        current = temp;
-    }
-}
+	current = temp;
+	}
+	}
 
