9798082248112b680a2053f843ba60baf900ba08
ikeali <ikechukwualisigwe01@mail.com>
2024-01-21 11:18:56 -0800

    104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -10,30 +10,31 @@
  * @index: The index of the root of the heap
  * @max_size: Maximum size of the array
  */
-void sift_down(int *array, size_t size, size_t index, size_t max_size)
-{
-    size_t root = index;
-    size_t left_child = 2 * index + 1;
-    size_t right_child = 2 * index + 2;
-    size_t largest = root;
+	void sift_down(int *array, size_t size, size_t index, size_t max_size)
+	{
+	size_t root = index;
+	size_t left_child = 2 * index + 1;
+	size_t right_child = 2 * index + 2;
+	size_t largest = root;
 
-    if (left_child < size && array[left_child] > array[largest])
-        largest = left_child;
+	if (left_child < size && array[left_child] > array[largest])
+	largest = left_child;
 
-    if (right_child < size && array[right_child] > array[largest])
-        largest = right_child;
+	if (right_child < size && array[right_child] > array[largest])
+	largest = right_child;
 
-    if (largest != root)
-    {
-        int temp = array[root];
-        array[root] = array[largest];
-        array[largest] = temp;
+	if (largest != root)
+	{
+	int temp = array[root];
 
-        print_array(array, max_size);
+	array[root] = array[largest];
+	array[largest] = temp;
 
-        sift_down(array, size, largest, max_size);
-    }
-}
+	print_array(array, max_size);
+
+	sift_down(array, size, largest, max_size);
+	}
+	}
 
 /**
  * heapify - Builds a heap from an array
@@ -41,13 +42,13 @@ void sift_down(int *array, size_t size, size_t index, size_t max_size)
  * @array: The array to be sorted
  * @size: Size of the array
  */
-void heapify(int *array, size_t size)
-{
-    ssize_t i;
+	void heapify(int *array, size_t size)
+	{
+	ssize_t i;
 
-    for (i = size / 2 - 1; i >= 0; --i)
-        sift_down(array, size, (size_t)i, size);
-}
+	for (i = size / 2 - 1; i >= 0; --i)
+	sift_down(array, size, (size_t)i, size);
+	}
 
 /**
  * heap_sort - Sorts an array of integers in ascending order using
@@ -56,24 +57,25 @@ void heapify(int *array, size_t size)
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
-void heap_sort(int *array, size_t size)
+	void heap_sort(int *array, size_t size)
 {
-    ssize_t i;
+	ssize_t i;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+	return;
 
-    heapify(array, size);
+	heapify(array, size);
 
-    for (i = size - 1; i > 0; --i)
-    {
-        int temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
+	for (i = size - 1; i > 0; --i)
+	{
+	int temp = array[0];
 
-        print_array(array, size);
+	array[0] = array[i];
+	array[i] = temp;
 
-        sift_down(array, (size_t)i, 0, size);
-    }
-}
+	print_array(array, size);
+
+	sift_down(array, (size_t)i, 0, size);
+	}
+	}
 
